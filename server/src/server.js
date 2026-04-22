a2d073162edb64752df7e20235159e64c0ee5af5
Ikerby <novoliva@gmail.com>
2026-04-22 16:18:40 +0200

    En teoría preparado para vercel

--- server/src/server.js
+++ server/src/server.js
@@ -64,13 +64,16 @@ const __filename = fileURLToPath(import.meta.url);	// Ruta d'aquest arxiu (servi
 const __dirname = path.dirname(__filename);			// Ruta de la carpeta on es troba aquest arxiu
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware para convertir JSON
 app.use(express.json());
 // permitir peticiones desde el cliente React en desarrollo
 app.use(cors({
-	origin: ['http://localhost:5173'],
+	origin: [
+		'http://localhost:5173',
+		process.env.FRONTEND_URL || 'http://localhost:5173'
+	],
 	credentials: true
 }));
 
